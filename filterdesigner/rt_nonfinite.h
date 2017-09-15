0222a6a43c9abb908055497589af1cebce62c66a
Travis Collins <travisfcollins@gmail.com>
2017-09-15 12:22:13 -0400

    Updated licensing

--- filterdesigner/rt_nonfinite.h
+++ filterdesigner/rt_nonfinite.h
@@ -1,11 +1,15 @@
 /*
- * Sponsored Third Party Support License -- for use only to support
- * products interfaced to MathWorks software under terms specified in your
- * company's restricted use license agreement.
- * File: rt_nonfinite.h
+ * Copyright (C) 2017 Analog Devices, Inc.
  *
- * MATLAB Coder version            : 3.2
- * C/C++ source code generated on  : 15-Aug-2017 18:43:56
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
  */
 
 #ifndef RT_NONFINITE_H
