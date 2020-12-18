bc1ac92a466720485d483e58723b3884c06c0072
Alexandru Ardelean <alexandru.ardelean@analog.com>
2020-12-18 16:02:31 +0200

    ci,windows: copy and adapt powershell scripts for win-builds
    Roughly adapted from libiio.
    
    Signed-off-by: Alexandru Ardelean <alexandru.ardelean@analog.com>

---
+++ CI/install_deps_win.ps1
@@ -0,0 +1,6 @@
+
+# Note: InnoSetup is already installed on Azure images; so don't run this step
+#       Running choco seems a bit slow; seems to save about 40-60 seconds here
+#choco install InnoSetup
+
+set PATH=%PATH%;"C:\Program Files (x86)\Inno Setup 6"
