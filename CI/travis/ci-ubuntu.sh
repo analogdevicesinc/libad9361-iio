95c5f4103c81fe84367f8a8926caa40ce201f8da
Raluca Chis <raluca.chis@analog.com>
2022-07-04 08:52:59 +0300

    CI:fix ARM builds
    Signed-off-by: Raluca Chis <raluca.chis@analog.com>

---
+++ CI/travis/ci-ubuntu.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -x
+uname -a
+DEBIAN_FRONTEND=noninteractive apt install -y make graphviz libaio-dev \
+	libavahi-client-dev libavahi-common-dev libusb-1.0-0-dev \
+	rpm tar bzip2 gzip libserialport-dev python3-pip
+dpkg -i /ci/build/*.deb
+python3 -m pip install pylibiio --no-binary :all:
+python3 -m pip install sphinx
+python3 -m pip install sphinx-rtd-theme
+
+echo "$PWD"
+
+mkdir -p build
+cd build
+cmake -DPYTHON_BINDINGS=ON -DENABLE_PACKAGING=ON -DDEB_DETECT_DEPENDENCIES=ON ..
+make && make package && make test
+make install
+ldconfig
+cd ../bindings/python
+pip3 install -r requirements_dev.txt
+python3 -m pytest -vs --skip-scan
