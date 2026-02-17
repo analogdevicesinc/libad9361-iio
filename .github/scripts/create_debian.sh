29f1894dbd0eb7a03db38dbd7c704577f41e9c65
Nicu Siderias <nicu.siderias@analog.com>
2026-02-17 10:02:09 +0200

    Add github workflow
    Create a new workflow + script that will build the app and deploy it for debian 13
    
    Change files permissions
    
    Signed-off-by: Nicu Siderias <nicu.siderias@analog.com>

---
+++ .github/scripts/create_debian.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+version=$1
+architecture=$(dpkg --print-architecture)
+
+source_code=$(basename "$PWD")
+
+# Use sudo only if not running as root
+if [ "$(id -u)" -eq 0 ]; then
+    apt-get update
+    apt-get install -y build-essential cmake libiio-dev python3 python3-pip python3-setuptools dh-python devscripts debhelper
+else
+    sudo apt-get update
+    sudo apt-get install -y build-essential cmake libiio-dev python3 python3-pip python3-setuptools dh-python devscripts debhelper
+fi
+
+# Replace placeholders inside the debian template files
+sed -i "s/@VERSION@/$version-1/" packaging/debian/changelog
+sed -i "s/@DATE@/$(date -R)/" packaging/debian/changelog
+sed -i "s/@ARCHITECTURE@/$architecture/" packaging/debian/control
+
+cp -r packaging/debian .
+
+rm -rf packaging
+
+pushd ..
+tar czf ${source_code}_${version}.orig.tar.gz \
+    --exclude='.git' \
+    --exclude='debian' \
+    $source_code
+popd
+
+debuild
