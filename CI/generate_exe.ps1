367413590c03355606df983ad88e2ee0db3aa3a6
Alin Opris <alin.opris@analog.com>
2026-09-01 17:23:11 +0300

    ci: Migrate azure-pipelines to github actions 	- Added builds for linux, linux arm, windows and macOS 	- Updated the building scripts 	- Updated cmake minimum version
    Signed-off-by: Alin Opris <alin.opris@analog.com>

--- CI/generate_exe.ps1
+++ CI/generate_exe.ps1
@@ -2,8 +2,8 @@
 $ErrorActionPreference = "Stop"
 $ErrorView = "NormalView"
 
-dir $env:BUILD_ARTIFACTSTAGINGDIRECTORY\Windows-VS-2019-x64
-iscc $env:BUILD_ARTIFACTSTAGINGDIRECTORY\Windows-VS-2019-x64\libad9361-iio.iss
+dir $env:BUILD_ARTIFACTSTAGINGDIRECTORY\Windows-VS-2022-x64
+iscc "/DSOURCE_DIR=$pwd" $env:BUILD_ARTIFACTSTAGINGDIRECTORY\Windows-VS-2022-x64\libad9361-iio.iss
 
 Get-ChildItem $env:BUILD_ARTIFACTSTAGINGDIRECTORY -Force -Recurse | Remove-Item -Force -Recurse
 cp C:\libad9361-setup.exe $env:BUILD_ARTIFACTSTAGINGDIRECTORY
